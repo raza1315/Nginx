cf31c7ff36b9c2292dbcb679e0e86591f95fdeab
raza1315 <razasfs2003@gmail.com>
2025-02-03 15:01:04 +0530

    made the server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express=require("express");
+const app=express();
+app.listent(3000,()=>{
+    console.log("server is up and running!");
+})
+app.get("/",(req,res)=>{
+	res.status(200).json({Message:"You have been proxied to localhost:3000/"});
+})
 
+app.get("/test",(req,res)=>{
+res.status(200).json(['testing','route']);
+})
